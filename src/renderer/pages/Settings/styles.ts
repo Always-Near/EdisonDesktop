6d8ce21582cb01d96139da192a1896d3e5d47485
NingYang <nyang@yipitdata.com>
2023-10-26 14:21:07 +0800

    update appTheme

--- src/renderer/pages/Settings/styles.ts
+++ src/renderer/pages/Settings/styles.ts
@@ -1,31 +1,16 @@
 import { css } from 'emotion'
 import { Theme as AppTheme } from '~/renderer/styles/theme'
 
-export const container = (appTheme: AppTheme) => css`
+export const container = ({ colors }: AppTheme) => css`
   width: 100%;
   height: 100%;
   display: flex;
-`
-
-export const content = (appTheme: AppTheme) => css`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  box-sizing: border-box;
-  padding: ${appTheme.spacing.medium}px;
-`
-
-export const paramGroup = (appTheme: AppTheme) => css`
-  display: flex;
-  align-items: center;
-  padding: ${appTheme.spacing.xsmall}px 0;
+  background: ${colors.primaryBackground};
 
   .title {
     width: 75px;
     flex: 0 0 auto;
-    color: ${appTheme.colors.textDark};
+    color: ${colors.textDark};
   }
 
   .items {
